a6f66acf01f57310fad0249756265189974e5b17
jeffersoncsw <je_wiltenburg@hotmail.com>
2021-02-06 00:43:58 -0300

    Projeto Phyton - calculos

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="./src/views")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if (request.method == "GET"):
+        return render_template("index.html")
+    else:
+        if (request.form["num1"] != "" and request.form["num2"] != ""):
+            num1 = request.form["num1"]
+            num2 = request.form["num2"]
+            if(request.form["opc"] == "soma"):
+                soma = int(num1) + int(num2)
+                return str(soma)
+            elif (request.form["opc"] == "subt"):
+                subt = int(num1) - int(num2)
+                return str(subt)
+            elif (request.form["opc"] == "mult"):
+                mult = int(num1) * int(num2)
+                return str(mult)
+            elif (request.form["opc"] == "divi"):
+                divi = int(num1) // int(num2)
+                return str(divi)
+        else:
+            return "Informe um valor para realizar o calculo escolhido."
+
+app.run(port=1910, debug=True)
